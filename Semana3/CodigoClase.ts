f5efdd4dbcb70afea231ef91ff6efbe6e39bce80
kapumota <claraa@uni.edu.pe>
2024-09-16 12:41:21 -0500

    Codigo de clase de typescript

--- Semana3/CodigoClase.ts
+++ Semana3/CodigoClase.ts
@@ -12,7 +12,8 @@ const typescriptChat: Chat = {name: 'profesor de typescript', model: 'gpt-3.5-tu
 function displayChat(chat: Chat) {
     console.log(`Chat: ${chat.name}, Model: ${chat.model}`);
 }
-/* Autor Mykyta Chernenko
+
+/*Author Mykyta Chernenko
 
 interface IChat {
     id: number;
@@ -52,6 +53,7 @@ class ChatResource implements IDatabaseResource<IChat> {
 }
 
 class UserResource implements IDatabaseResource<IUser> {
+
     async get(id: number): Promise<IUser | null> {
         return null;
     }
@@ -65,7 +67,6 @@ class UserResource implements IDatabaseResource<IUser> {
     }
 }
 
-
 class AuthenticationService {
     async isAuthenticated(token: string): Promise<boolean> {
         return true;
@@ -77,9 +78,10 @@ class ChatService {
     private userResource: UserResource = new UserResource();
     private authService: AuthenticationService = new AuthenticationService();
 
+
     async getChatMessages(chatId: number, token: string): Promise<IMessage[] | null> {
         if (!await this.authService.isAuthenticated(token)) {
-            throw new Error('Usuario no autenticado');
+            throw new Error('User no autenticado');
         }
 
         const userId = await this.userResource.getIdFromToken(token);
@@ -95,36 +97,40 @@ class ChatService {
 }
 
 
+const chatService = new ChatService();
+
 async function getChatMessages(chatId: number, token: string) {
     try {
         const messages = await chatService.getChatMessages(chatId, token);
         return {success: true, messages};
     } catch (error) {
-        return {success: false, message: 'Error interno del servidor'};
+        return {success: false, message: 'Internal server error'}
     }
 }
 
-function narrowToNumber(value: unknown): number { 
-    if (typeof value !== 'number') { 
-        throw new Error('Value no es un numero'); 
-    } 
-    return value; 
-} 
+function narrowToNumber(value: unknown): number {
+    if (typeof value !== 'number') {
+        throw new Error('Value no es un numero');
+    }
+    return value;
+}
+
+async function getChatMessagesWithNarrowing(chatId: unknown, req: { authorization: string }) {
+    const authToken = req.authorization;
+    const numberChatId = narrowToNumber(chatId);
+    const messages = await chatService.getChatMessages(numberChatId, authToken);
+
+    if (messages !== null) {
+        messages.map((message) => {
+            console.log(`Message ID: ${message.id}, Feedback: ${message.feedback?.trim() ?? "no feedback"}`);
+        });
+        return {success: true, messages}
+    } else {
+        return {success: false, message: 'Chat no encontrado o acceso denegado'}
+    }
+}
 
 
-async function getChatMessagesWithNarrowing(chatId: unknown, req: { authorization: string }) { 
-    const authToken = req.authorization; 
-    const numberChatId = narrowToNumber(chatId); 
-    const messages = await chatService.getChatMessages(numberChatId, authToken); 
-    if (messages !== null) { 
-        messages.map((message) => { 
-            console.log(Message ID: ${message.id}, Feedback: ${message.feedback?.trim() ?? "no feedback"}); 
-        }); 
-        return {success: true, messages} 
-    } else { 
-        return {success: false, message: 'Chat no encontrado o acceso denegado'} 
-    } 
-} 
 function getChatMessage(): IMessage | null {
     return null
 }
@@ -134,8 +140,7 @@ if (message !== null) {
     console.log(message.chatId)
 }
 
-console.log(`Message ID: ${message?.id}, Feedback: ${message?.feedback?.trim() ?? "no feedback"}`); 
-
+console.log(`Message ID: ${message?.id}, Feedback: ${message?.feedback?.trim() ?? "no feedback"}`);
 
 type NarrowFunction = (value: any) => number;
 
@@ -147,5 +152,239 @@ const logMessage: MapCallback = (message) => {
 // messages.map((message: IMessage) => {
 //     logMessage(message);
 // });
+
+
+interface IUser {
+    id: number;
+    name: string;
+    email: string;
+}
+
+type UserPreview = Pick<IUser, 'id' | 'name'>;
+type UserNamesById = Record<number, string>;
+type PartialIUser = Partial<IUser>;
+type RequiredIUser = Required<PartialIUser>;
+type UserWithoutEmail = Omit<IUser, 'email'>;
+type ReadonlyIUser = Readonly<IUser>;
+
+
+const userPreview: UserPreview = {id: 1, name: 'John'};
+const userNamesById: UserNamesById = {1: 'John', 2: 'Alice'};
+const userWithoutEmail: UserWithoutEmail = {id: 2, name: 'Alice'};
+const user: ReadonlyIUser = {id: 1, name: 'John', email: 'john@example.com'};
+// user.name = 'Alice';
+const partialUser: PartialIUser = {id: 1};
+const requiredUser: RequiredIUser = {id: 1, name: 'John', email: 'john@example.com'};
+
+// union types
+
+type MessageType = "user" | "assistant"
+
+interface IMessageWithType {
+    type: MessageType;
+    // other properties
+}
+
+type DbChatSuccessResponse = {
+    success: true;
+    data: IChat;
+};
+
+type DbChatErrorResponse = {
+    success: false;
+    error: string;
+};
+
+function getChatFromDb(chatId: string): DbChatSuccessResponse | DbChatErrorResponse {
+    const findChatById = (_: string) => ({} as IChat)
+    const chat = findChatById(chatId);
+    if (chat) {
+        return {
+            success: true,
+            data: chat,
+        };
+    } else {
+        return {
+            success: false,
+            error: "Chat no encontrado en la base de datos",
+        };
+    }
+}
+
+// Handling the response:
+const dbResponse = getChatFromDb("chat123");
+if (dbResponse.success === true) {
+    console.log("Chat data:", dbResponse.data);
+} else {
+    console.error("Error:", dbResponse.error);
+}
+
+// type intersections
+type IDBEntityWithId = {
+    id: number;
+};
+
+type IChatEntity = {
+    name: string;
+};
+
+type IChatEntityWithId = IDBEntityWithId & IChatEntity;
+
+const chatEntity: IChatEntityWithId = {
+    id: 1,
+    name: "Typescript tuitor",
+};
+
+// exntensible interfaces
+interface IMessageWithType extends IMessage {
+    type: MessageType;
+}
+
+const userMessage: IMessageWithType = {
+    id: 10,
+    chatId: 2,
+    userId: 1,
+    content: "Hello, world!",
+    createdAt: new Date(),
+    type: "user",
+};
+
+// OOP
+abstract class AbstractDatabaseResource {
+
+    constructor(protected resourceName: string) {
+    }
+
+    protected logResource(resource: { id: number }): void {
+        console.log(`[${this.resourceName}] Resource logged:`, resource);
+    }
+
+    abstract get(id: number): { id: number } | null
+
+    abstract getAll(): { id: number }[]
+
+    abstract addResource(resource: { id: number }): void;
+}
+
+class InMemoryChatResource extends AbstractDatabaseResource {
+    private resources: IChat[] = [];
+
+    constructor() {
+        super("chat");
+    }
+
+    get(id: number): IChat | null {
+        const resource = this.resources.find((item) => item.id === id);
+        return resource ? {...resource} : null;
+    }
+
+    getAll(): IChat[] {
+        return [...this.resources];
+    }
+
+    addResource(resource: IChat): void {
+        this.resources.push(resource);
+        this.logResource(resource);
+    }
+}
+
+class SqlChatResource extends AbstractDatabaseResource {
+
+    constructor() {
+        super("chat");
+        // Initialize SQL connection and setup here
+    }
+
+    addResource(resource: IChat): void {
+        // SQL-specific logic to add a resource
+        this.logResource(resource)
+    }
+
+    get(id: number): IChat | null {
+        // SQL-specific logic to select a chat
+        return null
+    }
+
+    getAll(): IChat[] {
+        // SQL-specific logic to select all chats
+        return []
+    }
+
+}
+
+const inMemoryChatResource = new InMemoryChatResource();
+const sqlChatResource = new SqlChatResource();
+
+const chat1: IChat = {
+    id: 1,
+    ownerId: 2,
+    messages: []
+};
+
+const chat2: IChat = {
+    id: 2,
+    ownerId: 1,
+    messages: []
+};
+
+inMemoryChatResource.addResource(chat1);
+sqlChatResource.addResource(chat2);
+
+const retrievedChat1 = inMemoryChatResource.get(1);
+if (retrievedChat1) {
+    console.log("Retrieved Chat from In-Memory Resource:", retrievedChat1);
+} else {
+    console.log("Chat not found in In-Memory Resource");
+}
+
+const retrievedChat2 = sqlChatResource.get(2);
+if (retrievedChat2) {
+    console.log("Retrieved Chat from SQL Resource:", retrievedChat2);
+} else {
+    console.log("Chat not found in SQL Resource");
+}
+
+// generics
+
+function printValue<T>(value: T): void {
+    console.log(value);
+}
+
+printValue<number>(123);
+printValue<string>("Hello");
+
+class GenericsInMemoryResource<T extends { id: number }> extends AbstractDatabaseResource {
+    private resources: T[] = [];
+
+    constructor(resourceName: string) {
+        super(resourceName);
+    }
+
+    get(id: number): T | null {
+        const resource = this.resources.find((item) => item.id === id);
+        return resource ? {...resource} : null;
+    }
+
+    getAll(): T[] {
+        return [...this.resources];
+    }
+
+    addResource(resource: T): void {
+        this.resources.push(resource);
+        this.logResource(resource);
+    }
+}
+
+const userInMemoryResource = new GenericsInMemoryResource<IUser>('user')
+const chatInMemoryResource = new GenericsInMemoryResource<IChat>('chat')
+
+userInMemoryResource.addResource({id: 1, name: 'Admin', email: 'admin@admin.com'});
+chatInMemoryResource.addResource({id: 10, ownerId: userInMemoryResource.get(1)!.id, messages: []});
+
+function fetchData(): Promise<string> {
+    return new Promise((resolve) => {
+        setTimeout(() => resolve("Data Fetched"), 1000);
+    });
+}
 
 */
